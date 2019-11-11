ba7ddb81daebfcc4687cd435be0f7b880261adf5
rumyantsev-ad <rumyantsev-ad@ranepa.ru>
2019-11-11 17:53:37 +0300

    commit content

---
+++ index.php
@@ -0,0 +1,121 @@
+<html>
+    <head>
+    <meta charset="utf-8">
+    <!-- Настройка viewport -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Подключаем Bootstrap CSS -->
+     <link rel="stylesheet" 
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+        crossorigin="anonymous"
+     > 
+     <link rel="stylesheet" href="./css/style.css">
+     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/webfonts/fa-solid-900.woff2">
+    </head>
+    <body>
+        <div class='container'>
+            <header> 
+                <div class='col-sm-1'> 
+                    <span class='container__SH'>
+                        SH
+                    </span>
+                </div>
+                <nav class='col-sm-9'>
+                    <a> Женцинам </a>
+                    <a> Мужчинам </a>
+                    <a> Детям </a>
+                    <a> Новинки </a>
+                    <a> О нас </a>
+                </nav>
+                <div class='col-sm-2'>
+                    <span class='container__enter'> 
+                        <img src='.\img\icons\account.png'>  
+                        Войти 
+                    </span>
+                    <span class='container__basket'>
+                       <img src='.\img\icons\bascet.png'>
+                        Корзина 
+                    </span>
+                </div>    
+            </header>
+            <main> 
+                <div class='wraper'> 
+
+                    <h1>НОВЫЕ ПООСТУПЛЕНИЯ ВЕСНЫ</h1>
+                    <div class='wraper__slogan'>Мы подготовили для Вас лучшие новинки сезона</div>
+                    <div class='wraper__look-new'> 
+                            <div class='wraper__look-new__item'> 
+                                Посмотреть новинки
+                            </div>  
+                    </div>
+
+                    <div class='wraper__gooods-block'>
+
+                        <div class='wraper__gooods-block__block col-sm-3'>
+                            <div class='wraper__gooods-block__block__2block'></div>
+                            <div class='wraper__gooods-block__block__1block'></div>
+                        </div>
+
+                        <div class='wraper__gooods-block__block col-sm-3'>
+                            <div class='wraper__gooods-block__block__1block'></div>
+                            <div class='wraper__gooods-block__block__1block'></div>
+                            <div class='wraper__gooods-block__block__1block'></div>
+                        </div>
+
+                        <div class='wraper__gooods-block__block col-sm-3'>
+                            <div class='wraper__gooods-block__block__1block'></div>
+                            <div class='wraper__gooods-block__block__1block'></div>
+                            <div class='wraper__gooods-block__block__1block'></div>
+                        </div>
+
+                        <div class='wraper__gooods-block__block col-sm-3'>
+                            <div class='wraper__gooods-block__block__1block'></div>
+                            <div class='wraper__gooods-block__block__2block'></div>
+                        </div>
+
+                    </div>
+                    
+                    <div class='wraper__bottom-info'>
+                        <div>БУДЬ ВСЕГДА В КУРСЕ ВЫГОДНЫХ ПРЕДЛОЖЕНИЙ</div>
+                        <div>подписывайся и следи за новинками и выгодными предложениями.</div>
+
+                        <form>
+                            <input type='text' placeholder='email'/>
+                            <input type='submit' value='Записаться'/>
+                        </form>
+                        <span id='system-message'></span>
+                    </div>
+                   
+                </div> 
+            </main>
+            <footer> 
+                <div class='footer-info'>  
+                    <div class='col-sm-4'>
+                        КОЛЛЕКЦИИ
+                        <div>Женцинам</div>
+                        <div>Мужчинам</div>
+                        <div>Детям</div>
+                        <div>Новинки</div>
+                    </div>
+                    <div class='col-sm-4'>
+                        МАГАЗИН
+                        <div>О нас</div>
+                        <div>Доставка</div>
+                        <div>Работай с нами</div>
+                        <div>Контакты</div>
+                    </div>
+                    <div class='col-sm-4'>
+                        МЫ В СОЦИАЛЬНЫХ СЕТЯХ
+                        <div>Сайт разработан в INordic.ru</div>
+                        <div>2018 . Все права защищены</div>
+                        <div class='footer-info__icons-social'>
+                            <div class='footer-info__icons-social__item'> <img src='#'> </div>
+                            <div class='footer-info__icons-social__item'> <img src='#'> </div>
+                            <div class='footer-info__icons-social__item'> <img src='#'> </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </body>
+</html>    
